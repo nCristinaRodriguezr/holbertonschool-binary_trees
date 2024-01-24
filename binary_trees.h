f9711177983e62eb0e38a5e715916a18dc4b2ca6
Nataly Rodriguez <ncristina.rodriguezr@gmail.com>
2024-01-24 14:33:36 -0800

    fixing structure

--- binary_trees.h
+++ binary_trees.h
@@ -13,7 +13,7 @@ typedef struct binary_tree_t
 	struct binary_tree_t *parent;
 	struct binary_tree_t *left;
 	struct binary_tree_t *right;
-};
+} binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif
