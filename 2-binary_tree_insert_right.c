a8d9282d8ce4ddf150bf95e86104467b3123dbc9
Nataly Rodriguez <ncristina.rodriguezr@gmail.com>
2024-01-24 16:56:36 -0800

    point 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
- * binary_tree_insert_rihgt- This function makes a new node right
+ * binary_tree_insert_right- This function makes a new node right
  * with the conditions of users
  * @value: is integer number
  * @parent: is a pointer
@@ -17,12 +17,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 	new_node = binary_tree_node(parent, value);
-	if (parent->rihgt != NULL)
+	if (parent->right != NULL)
 	{
-		new_node->rihgt = parent->rihgt;
-		parent->rihgt->parent = new_node;
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
 	}
-	parent->rihgt = new_node;
+	parent->right = new_node;
 	return (new_node);
 }
 
