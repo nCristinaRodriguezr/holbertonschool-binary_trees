13ea402eba9971255ace2de96acb9242e3739761
Nataly Rodriguez <ncristina.rodriguezr@gmail.com>
2024-01-25 08:52:07 -0800

    point 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -14,7 +14,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
-	func(tree->value);
+	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
