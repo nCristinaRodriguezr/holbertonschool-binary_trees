52f6348c5f0b76108608d994bebb297ab2d76f9b
Nataly Rodriguez <ncristina.rodriguezr@gmail.com>
2024-01-24 17:37:09 -0800

    point 3

--- binary_trees.h
+++ binary_trees.h
@@ -18,5 +18,5 @@ typedef struct binary_tree_t
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-binary_tree_delete(binary_tree_t *tree)
+void binary_tree_delete(binary_tree_t *tree);
 #endif
