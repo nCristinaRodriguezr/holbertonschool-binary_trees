27fa111bfd5c8f07b32c768f3e83a6bec6d1b03a
Nataly Rodriguez <ncristina.rodriguezr@gmail.com>
2024-01-25 17:42:19 -0800

    point 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -19,7 +19,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	altura_izquierda = binary_tree_height(tree->left);
 	altura_derecha = binary_tree_height(tree->right);
 
-	if (altura_ezquierda == 0 && altura_derecha == 0)
+	if (altura_izquierda == 0 && altura_derecha == 0)
 	{
 		return (0);
 	}
