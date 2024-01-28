b74ca841183620997ad6985ef5432bb05cb1ef33
Nataly Rodriguez <ncristina.rodriguezr@gmail.com>
2024-01-28 14:14:10 -0800

    point 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,7 +21,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree->left == NULL && tree->right == NULL)
 	{
-		return (0);
+		return (1);
 	}
 	if (altura_izquierda > altura_derecha)
 	{
