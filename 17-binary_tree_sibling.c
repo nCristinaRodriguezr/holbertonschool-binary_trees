2982f7a97a3cb1eadf148132ba1632fb2daa7215
Nataly Rodriguez <ncristina.rodriguezr@gmail.com>
2024-01-30 08:16:15 -0800

    point 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,11 +9,13 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	binary_tree_t *parent;
+
 	if (node == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
-	binary_tree_t *parent = node->parent;
+	parent = node->parent;
 
 	if (parent->left == node)
 	{
